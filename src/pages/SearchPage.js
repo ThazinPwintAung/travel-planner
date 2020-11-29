9460997ab269973b6560c9c18458cb7f3ce6ba37
Sai Han <thesaihan@gmail.com>
2020-11-29 13:39:14 +0630

    Add fadeUp animation in SearchPage

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import ResultWrapper from '../components/ResultWrapper'
-import SearchHeader from '../components/SearchHeader'
+import React from "react";
+import ResultWrapper from "../components/ResultWrapper";
+import SearchHeader from "../components/SearchHeader";
+import "./SearchPage.css";
 
 const SearchPage = () => {
-    return (
-        <div>
-            <SearchHeader />
-            <ResultWrapper />
-        </div>
-    )
-}
+  return (
+    <div className="SearchPage">
+      <SearchHeader />
+      <ResultWrapper />
+    </div>
+  );
+};
 
-export default SearchPage
+export default SearchPage;
