efe8ef421beddeefd00ee3b5d858b34cede4f469
ThazinPwintAung <tzpaung16ceit@ytu.edu.mm>
2020-12-20 17:49:39 +0630

    responsive card

--- src/components/ActivityCard.js
+++ src/components/ActivityCard.js
@@ -4,12 +4,12 @@ import {
   CardContent,
   CardMedia,
   IconButton,
-  makeStyles,
   Typography,
   Box,
   Button,
 } from "@material-ui/core";
 import React, { useState } from "react";
+import "./ActivityCard.css";
 import {
   addToBoard,
   removeFromBoard,
@@ -18,46 +18,6 @@ import { connect, useSelector } from "react-redux";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import StarBorderIcon from "@material-ui/icons/StarBorder";
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    width: "100%",
-    margin: 20,
-    position: "relative",
-  },
-  "& > * + *": {
-    marginTop: theme.spacing(2),
-  },
-  details: {
-    display: "flex",
-    flexDirection: "column",
-  },
-  content: {
-    flex: "1 0 auto",
-  },
-  cover: {
-    minWidth: 300,
-  },
-  typography: {
-    fontSize: 16,
-    fontWeight: 900,
-    marginRight: 30,
-    color: "#282d36",
-  },
-  snackbar: {
-    position: "absolute",
-    top: 0,
-  },
-  book: {
-    fontSize: 14,
-    fontWeight: 900,
-    color: "#0a4e75",
-  },
-  star: {
-    color: "#282d36",
-  },
-}));
 
 const ActivityCard = ({
   actId,
@@ -71,7 +31,6 @@ const ActivityCard = ({
     state.board.board.find((act) => act.id === actId)
   );
   const [count, setCount] = useState(1); //to remove fav on another click
-  const classes = useStyles();
   const imgSrc = actList.pictures[0];
 
   const addToTravelboard = () => {
@@ -87,28 +46,29 @@ const ActivityCard = ({
   };
 
   return (
-    <Card className={classes.root}>
-      <CardMedia
-        className={classes.cover}
-        image={imgSrc}
-        title={actList.name}
-      />
-      <div className={classes.details}>
-        <CardContent className={classes.content}>
-          <Typography component="h6" variant="h6">
+    <Card className="Card">
+      <CardMedia className="cover" image={imgSrc} title={actList.name} />
+      <div className="details">
+        <CardContent className="content">
+          <Typography component="h6" variant="h6" className="header">
             {actList.name}
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
+          <Typography
+            variant="body2"
+            color="textSecondary"
+            component="p"
+            className="desc"
+          >
             {actList.shortDescription}
           </Typography>
           <Box display={"flex"} alignItems={"center"} mt={2}>
-            <Typography className={classes.typography}>
+            <Typography className="typography">
               {actList.price.amount}&nbsp;
               {actList.price.currencyCode}
             </Typography>
             <Box display={"flex"} alignItems={"center"}>
-              <StarBorderIcon className={classes.star} />
-              <Typography className={classes.typography}>
+              <StarBorderIcon className="star" />
+              <Typography className="typography">
                 &nbsp;{parseFloat(actList.rating).toFixed(1)}
               </Typography>
             </Box>
@@ -125,7 +85,7 @@ const ActivityCard = ({
               )}
             </IconButton>
             <Button size="small">
-              <a href={actList.bookingLink} className={classes.book}>
+              <a href={actList.bookingLink} className="book">
                 Book Here!
               </a>
             </Button>
